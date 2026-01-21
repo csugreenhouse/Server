59168bc8951a3e90dc1ffacf1c614bd5d4a96b28
agcam <carv@colostate.edu>
2026-01-21 13:24:11 -0700

    cleaned up file paths

---
+++ old/old/helper.py
@@ -0,0 +1,83 @@
+import cv2
+import matplotlib.pyplot as plt
+
+def find_best_bias(image, axy, axx, plant_height):
+    bestbias = (0,0,0)
+    height, width, _ = image.shape
+    best_difference=100
+    for rbias in range(90,255,15):
+        for gbias in range(90, 255, 15):
+            for bbias in range (90,255, 15):
+                print (f"testing bias ({rbias},{gbias},{bbias})")
+                found = False
+                guessed_height = 0
+                for y in range (height-1, axy, -1):
+                    for x in range (axx, width):
+                            b, g, r = image[y, x]
+                            if r < rbias and g > gbias and b < bbias:
+                                guessed_height = 17+((y - axy)  / pixels_per_cm)
+                                found = True
+                                break
+                            if 'found' in locals():
+                                break
+                if abs(plant_height-guessed_height)<best_difference:
+                    best_difference = abs(plant_height-guessed_height)
+                    best_bias = (rbias,gbias,bbias)
+                    print (f"new accuracy: {(guessed_height-plant_height)/plant_height}")
+    return (rbias,gbias,bbias)
+
+
+
+# Load image
+image = cv2.imread("greenhouse/plant.jpg")
+if image is None:
+    print("Image not found") 
+else:
+    
+    # Parameters
+    #these are all in pixels
+    # Where the plant image first appears in the image.
+    axy = 171
+    #can be anything, but the farthest leaf of the plant should be okay
+    axx = 0
+    #pixels per cm
+    pixels_per_cm = 106
+    # bias for how the program finds the color of the plant 
+    rbias = 150
+    gbias = 175
+    bbias = 150
+    
+    print(find_best_bias(image, axy, axx, 29))
+    
+    
+    image = cv2.flip(image, 0)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    
+    height, width, _ = image.shape
+    plt.imshow(image_rgb) 
+    plt.axis('on')
+    
+    plt.xlim(axx, width)
+    plt.ylim(axy, height)
+    
+    M = pixels_per_cm
+    for y in range(axy, height, M):
+        plt.axhline(y=y, color='blue', linestyle='--', linewidth=1)
+    
+    for y in range (height-1, axy, -1):
+        print (f"Scanning row {y}")
+        for x in range (axx, width):
+            b, g, r = image[y, x]
+            if r < rbias and g > gbias and b < bbias:
+                print(f"Found green pixel at ({x}, {y})")
+                print(f"Color values: R={r}, G={g}, B={b}")
+                print(f"Height in cm: {17+((y - axy)  / pixels_per_cm):.2f}")
+                plt.scatter([x], [y], c="red", s=40, marker="o")
+                found = True
+                break
+        if 'found' in locals():
+            break       
+        
+    plt.savefig("greenhouse/output.png", bbox_inches="tight")
+
---
+++ old/old/requestor/requestor.py
@@ -0,0 +1,58 @@
+
+from datetime import datetime
+import time
+import tempfile
+import requests
+# below i will import processing functions
+import sys
+sys.path.append('/srv/samba/Server')
+import cv2
+import numpy as np
+
+import plant_requests.height_request.height_request as hr
+import plant_requests.utils.graph_util as graph
+import plant_requests.utils.database_util as database_util
+
+
+def get_image(url):
+    try:
+        response = requests.get(url,timeout=10)
+        response.raise_for_status()
+        image = response.content
+        return cv2.imdecode(np.frombuffer(image, np.uint8), cv2.IMREAD_COLOR)
+    except requests.exceptions.RequestException as e:
+        print(f"failed to get image: {e}")
+        return None
+    
+def request_organizer(reference_tags, camera_parameters, image):
+    height_request_reference_tags = []
+    area_request_reference_tags = []
+    for reference_tag in reference_tags:
+        if reference_tag.get("request_type").constains("height_estimation"):
+            height_request_reference_tags.append(reference_tag)
+        elif reference_tag.get("request_type").constains("area_estimation"):
+            area_request_reference_tags.append(reference_tag)
+    height_estimations, height_estimations_info = hr.height_request(
+        image=image,
+        reference_tags=height_request_reference_tags,
+        camera_parameters=camera_parameters
+    )
+    # area_estimations, area_estimations_info = ar.area_request(
+    #     image=image,
+    #     reference_tags=area_request_reference_tags,
+    #     camera_parameters=camera_parameters
+    # )
+    return (height_estimations, height_estimations_info),(None, None)
+
+# SET WHICH CAMERAS TO USE. THIS WILL EVENTUALLY COME FROM THE DATABASE  
+camera_ids = [1]
+camera_IPs = [database_util.get_camera_ip(camera_id) for camera_id in camera_ids]
+
+#get the image from the first camera
+
+image = get_image(f"http://{camera_IPs[0]}/capture")
+#camera_parameters = hr.info_getter_util.qeury_camera_parameters(camera_ids[0])
+#reference_tags = hr.info_getter_util.get_reference_tags(image, camera_parameters)
+
+
+
